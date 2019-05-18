7b2e275c08d06ae86e2a0d56bfeef0f58b5ff9d5
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-05-18 12:08:52 +0500

    Refactor default component

--- src/Components/Default.js
+++ src/Components/Default.js
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Default extends Component {
   render() {
+    console.log(this.props)
     return (
       <React.Fragment>
-          <h3>Page not found</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto text-center text-title-text-uppercase pt-5">
+              <h1 className="display-3">404 Error</h1>
+              <h2>The requested url <span className={'text-danger'}>{this.props.location.pathname}</span> wasn't found</h2>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
